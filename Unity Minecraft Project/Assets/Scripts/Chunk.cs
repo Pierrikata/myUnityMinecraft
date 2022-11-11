29107911a9cf7d615c8b3d78e30323e71d48a35f
Peter Siri <44070829+Pierrikata@users.noreply.github.com>
2022-11-11 14:25:05 -0800

    CreatingFirstVoxelPartOne

---
+++ Unity Minecraft Project/Assets/Scripts/Chunk.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+    void Start()
+    {
+        
+    }
+}
